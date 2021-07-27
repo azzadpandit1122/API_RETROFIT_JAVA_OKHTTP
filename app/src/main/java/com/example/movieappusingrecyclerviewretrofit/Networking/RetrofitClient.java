0e89f2a487ff03c164a754c43f1f382d11a6da74
azzadpandit <azzdpandit0@gmail.com>
2021-07-27 22:43:40 +0530

    Fist of my Recycler code on Retrofit

--- app/src/main/java/com/example/movieappusingrecyclerviewretrofit/Networking/RetrofitClient.java
+++ app/src/main/java/com/example/movieappusingrecyclerviewretrofit/Networking/RetrofitClient.java
@@ -9,28 +9,30 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static RetrofitClient retrofitClient;
     private static Retrofit retrofit;
-//    private static String BASE_URL ="https://run.mocky.io/";
+    //    private static String BASE_URL ="https://run.mocky.io/";
     private static String BASE_URL = "http://color.proinnovativesoftware.co/";
     //Api/Rebliss/GetMarkAttendanceUserInfo/0
 
-    private RetrofitClient()
-    {
-
+    private RetrofitClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient httpClient = new OkHttpClient();
+        httpClient.interceptors().add(logging);  // <-- this is the important line!
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
                 .build();
     }
 
-    public static synchronized RetrofitClient getInstance(){
-        if (retrofitClient == null)
-        {
-            retrofitClient= new RetrofitClient();
+    public static synchronized RetrofitClient getInstance() {
+        if (retrofitClient == null) {
+            retrofitClient = new RetrofitClient();
         }
         return retrofitClient;
     }
-    public Api getApi()
-    {
+
+    public Api getApi() {
         return retrofit.create(Api.class);
     }
 
